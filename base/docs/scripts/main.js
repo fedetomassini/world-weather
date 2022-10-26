2fcadeedba67e713899f3578ea4f0a542670186b
Federico Tomassini <fedetomassini2@gmail.com>
2022-10-26 20:35:24 -0300

    minor changes

--- base/docs/scripts/main.js
+++ base/docs/scripts/main.js
@@ -35,7 +35,7 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+        /* document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"; */
     },
     search: function() {
         this.fetchWeather(document.querySelector(".search-bar").value);
