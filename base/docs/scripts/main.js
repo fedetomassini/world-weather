1e4ca0e310782e23b55d780664da8d31fff95c29
Federico Tomassini <fedetomassini2@gmail.com>
2022-10-27 18:23:27 -0300

    70% language selector

--- base/docs/scripts/main.js
+++ base/docs/scripts/main.js
@@ -9,16 +9,17 @@ let weather = {
                     Swal.fire({
                         icon: 'error',
                         title: 'Oops...',
-                        text: 'No weather found!',
+                        text: 'No weather found! Try another',
                         showConfirmButton: false,
-                        timer: 1500,
+                        timer: 2750,
                         timerProgressBar: true,
                         allowEscapeKey: false,
                         allowEnterKey: false,
                         allowOutsideClick: false,
                         heightAuto: true,
                         width: 425,
-                    })
+                    });
+                    console.error("Invalid entry");
                 }
                 return response.json();
             })
@@ -39,53 +40,69 @@ let weather = {
         const {
             speed
         } = data.wind;
-        document.querySelector(".city").innerText = name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText = "Humedad: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Velocidad del viento: " + speed + " km/h";
+        $('.city').html(name);
+        $('.icon').html("https://openweathermap.org/img/wn/" + icon + ".png");
+        $('.description').html(description);
+        $('.temp').html(temp + "°C");
+        $(".humidity").html("Humedad: " + humidity + "%");
+        $(".wind").html("Velocidad del viento: " + speed + " km/h");
         document.querySelector(".weather").classList.remove("loading");
         /* document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"; */
 
-        ///////////// Language Selector \\\\\\\\\\\\\
-        ///////////// Work In Progress !NO TOCAR¡ \\\\\\\\\\\\\
-        let html_lang = document.getElementsByTagName("html")[0].getAttribute("lang");
-
+            ////////////////////////////////////////
+        ////////////// Language Selector \\\\\\\\\\\\\
+            ////////////////////////////////////////
+       let html_lang = document.getElementsByTagName("html")[0].getAttribute("lang");
         const languages = {
             "es": "español",
             "en": "english",
         }
-        if(html_lang === "es"){
-            console.group("Language Selector");
-            console.info("Idioma del sitio web -> " + languages.es);
-            document.querySelector(".search-bar").placeholder="Busca una ciudad...";
-            document.querySelector(".humidity").innerText = "Humedad: " + humidity + "%";
-            document.querySelector(".wind").innerText = "Velocidad del viento: " + speed + " km/h";
-        }else if(html_lang === "en"){
-            console.group("Language Selector");
-            console.info("Website language -> " + languages.en);
-            document.querySelector(".search-bar").placeholder="Search a city...";
-            document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-            document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h";
+        document.querySelector(".languageSelector.english").addEventListener("click", function english(){
+                document.getElementsByTagName("html")[0].setAttribute("lang", "en");
+                console.group("Language Selector");
+                console.info("Website language -> " + languages.en);
+                $('.search-bar').attr('placeholder', "Search a city...");
+                $(".humidity").html("Humidity: " + humidity + "%");
+                $(".wind").html("Wind Speed: " + speed + " km/h");
+                localStorage.setItem("language", "english");
+                state:{
+                    lang: localStorage.setItem('lang', html_lang = "en");
+                }
+        });
+        document.querySelector(".languageSelector.spanish").addEventListener("click", function spanish(){
+                document.getElementsByTagName("html")[0].setAttribute("lang", "es");
+                console.group("Language Selector");
+                console.info("Idioma del sitio web -> " + languages.es);
+                $('.search-bar').attr('placeholder', "Busca una ciudad...");
+                $(".humidity").html("Humedad: " + humidity + "%");
+                $(".wind").html("Velocidad del viento: " + speed + " km/h");
+                localStorage.setItem("language", "spanish");
+                state:{
+                    lang: localStorage.setItem('lang', html_lang = "es");
+                }
+        });
+        if(localStorage.getItem("language" || "lang") === "spanish" || "es"){
+            /* return; */
+        }else if(localStorage.getItem("language" || "lang") === "english" || "en"){  
+            /* return; */
         }
+        ////////////////////////////////////////
     },
-    search: function() {
+    search: function(){
         this.fetchWeather(document.querySelector(".search-bar").value);
     },
 };
 
-document.querySelector(".search button").addEventListener("click", function() {
+document.querySelector(".search button").addEventListener("click", function(){
     weather.search();
 });
 
-document
-    .querySelector(".search-bar")
-    .addEventListener("keyup", function(event) {
-        if (event.key == "Enter") {
-            weather.search();
-        }
-    });
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if (event.key == "Enter"){
+        weather.search();
+    }
+});
 
 /* Default */
 weather.fetchWeather("Tokyo");
+console.clear();
